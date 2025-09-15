4e689733975b26d24fa8a148d6b2bc2e69b0a100
Olek-B <u57_alebla_waw@technischools.com>
2025-09-15 14:10:31 +0200

    cleanup

--- src/routes/messages.rs
+++ src/routes/messages.rs
@@ -5,7 +5,3 @@ use crate::{auth::create_jwt, chat::ws_handler, db::postgres::PostgresDb, AppSta
 pub fn router() -> Router<AppState<PostgresDb>> {
     Router::new().route("/listen", any(ws_handler))
 }
-
-async fn login_handler() -> impl IntoResponse {
-    create_jwt(1)
-}
